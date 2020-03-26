70dbc8fedfb360607656b0b9d68d67e0b3cf5d7f
Simon Nordberg <simon.nordberg@gmail.com>
2020-03-26 21:15:37 +0100

    Fix analytics

--- app.js
+++ app.js
@@ -117,20 +117,22 @@ $(function () {
         return false;
     });
 
-    const initAnalytics = function() {
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','tzga');
+    const isAnalyticsAvailable = function() {
+        return typeof ga === 'function';
+    }
 
-        tzga('create', 'UA-59609997-2', 'auto');
-        tzga('send', 'pageview');
+    const initAnalytics = function() {
+        if (isAnalyticsAvailable()) {
+            ga('create', 'UA-59609997-2', 'auto', 'hrTracker');
+        }
     };
 
     const trackSubmit = function(aet, ant, hrmax) {
-        let url = document.location.pathname + `?hrmax=${hrmax}&ant=${ant}&aet=${aet}`
-        tzga('send', 'pageview', url);
+        if (isAnalyticsAvailable()) {
+            let url = document.location.pathname + `?hrmax=${hrmax}&ant=${ant}&aet=${aet}`
+            ga('hrTracker.send', 'pageview', url);
+        }
     };
 
-    //initAnalytics();
+    initAnalytics();
 });
